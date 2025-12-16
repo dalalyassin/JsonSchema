5e956bc01e7f35e90e109d5f816b6baf66984a36
Dalal <dalaalyassin@gmail.com>
2025-12-16 15:10:49 +0200

    parser

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from schemas.llm_assistant import input, Response
 from openai import Client
 import os
 from dotenv import load_dotenv
-
+from langchain_core.output_parsers import PydanticOutputParser
 load_dotenv()
-
-
+ 
+ 
 def prompt_builder(input: input) -> str:
     prompt = f"tell me about {input.input}"
     return prompt
-
-
+ 
+ 
 def get_response(prompt: str) -> Response:
+    pydantic_output_parser = PydanticOutputParser(pydantic_object=Response)
+    format_instructions = pydantic_output_parser.get_format_instructions()
     client = Client(api_key=os.getenv("OPENAI_API_KEY"))
     response = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role": "user", "content": prompt}]
+        messages=[{"role": "user", "content": prompt + format_instructions}]
     )
-    return Response(response=response.choices[0].message.content, ).model_dump_json()
-    
-
+    return pydantic_output_parser.parse(response.choices[0].message.content)
+   
+ 
 prompt = prompt_builder(input(input="how are you?"))
 print(get_response(prompt))
+ 
